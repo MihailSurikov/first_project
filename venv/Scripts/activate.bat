e6489ff4d3cd59c085f8c99866efeb5f78872d1a
Михаил Суриков <surikovmiha@yandex.ru>
2025-07-28 21:51:34 +0300

    add venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Dev\first_project\venv
+set VIRTUAL_ENV=D:\dev\first_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
