7a4d704398ba34bbb1871925bb024dc1ee6c664f
Михаил Суриков <surikovmiha@yandex.ru>
2025-07-28 21:32:18 +0300

    Practica

--- first_project.py
+++ first_project.py
@@ -1,6 +1,10 @@
 print('Привет, Практикум!')
 print('Познаём GIT!')
 # Практикуем  работу с  GIT
-print('Запоминай всё!')
-# Важно  следить и комитить 
+print('Запоминай всё! Учи')
+text_pr = 'Ученье свет, а не ученье тьма!'
+# Важно  следить и коми
+def print_sentences(all):
+    print(str(all))
+print_sentences(text_pr)    
 
